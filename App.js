6d0b9ef4a40b567f547395f4571766eb63613323
Saravanan <sharvanan7944@gmail.com>
2020-02-29 21:25:29 +0530

    navigation completed

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import { ApplicationProvider } from '@ui-kitten/components';
 import { mapping, light as lightTheme } from '@eva-design/eva';
-import HomeScreen from './app/HomeScreen.js'
+import HomeScreen from './app/HomeScreen.js';
 
 export default class App extends Component {
+  constructor(state, props){
+    super(state);
+    this.state={
+      
+    }
+  }
   render(){
     return (
       <ApplicationProvider mapping={mapping} theme={lightTheme}>
-        <HomeScreen />
+        <HomeScreen ref="homeScreen"/>
       </ApplicationProvider>
     );
   }
-  onActionSelected= (position)=>{
-    if (position === 0) { // index of 'Settings'
-      showSettings();
-    }
-  }
 };
 
-// const styles = StyleSheet.create({
-  
-// });
--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -1,18 +1,49 @@
 import React, {Component} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Drawer as UIKittenDrawer, Layout, Text } from '@ui-kitten/components';
+import ActionBar from 'react-native-action-bar';
+import {
+  StyleSheet,
+  Image,
+  View
+} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
-const UsersScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+const ActionBarLayout = ({ navigation }) => {
+  const openDrawer = ()=>{
+    console.log(navigation);
+    navigation.openDrawer();
+  };
+
+  return(
+    <ActionBar
+      containerStyle={styles.bar}
+      title={'React-native-action-bar Example'}
+      leftIconName={'menu'}
+      onLeftPress={openDrawer}
+      onTitlePress={() => console.log('Title!')}
+      rightIcons={[
+          {
+              name: 'flag',
+              onPress: () => console.log('Right Flag !'),
+          },
+      ]}
+    />
+  )
+}
+
+const UsersScreen = ({navigation}) => (
+  <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+    <ActionBarLayout navigation={navigation}/>
     <Text category='h1'>USERS</Text>
   </Layout>
 );
 
-const OrdersScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+const OrdersScreen = ({navigation}) => (
+  <Layout style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+    <ActionBarLayout navigation={navigation}/>
     <Text category='h1'>ORDERS</Text>
   </Layout>
 );
@@ -22,22 +53,43 @@ const DrawerContent = ({ navigation, state }) => {
   const onSelect = (index) => {
     navigation.navigate(state.routeNames[index]);
   };
+  const profileImage ='https://via.placeholder.com/150';
 
   return (
-    <UIKittenDrawer
-      data={[{ title: 'Home' }, { title: 'Settings' }]}
-      selectedIndex={state.index}
-      onSelect={onSelect}
-    />
+    <Layout style={{flex: 1, flexDirection: 'column', alignItems:"stretch", justifyContent:"flex-start"}}>
+      <Layout style={styles.title}>
+        <Image
+          source={{ uri: profileImage }}
+          style={styles.sideMenuProfileIcon}
+        />
+        <Layout style={styles.list}>
+          <Text style={{fontWeight:'bold'}}>Saravanan</Text>
+          <Text style={{fontSize:12}}>sharvanan7944@gmail.com</Text>
+          <Text style={{fontSize:12, color: '#27ae60'}}>ID: #7944</Text>
+        </Layout>
+      </Layout>
+      <Layout style={styles.navigation}>
+        <UIKittenDrawer
+          data={[{ title: 'Home' }, { title: 'Settings' }]}
+          selectedIndex={state.index}
+          onSelect={onSelect}
+        />
+      </Layout>
+    </Layout>
   );
 };
 
-export const DrawerNavigator = () => (
-  <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>}>
-    <Drawer.Screen name='Users' component={UsersScreen}/>
-    <Drawer.Screen name='Orders' component={OrdersScreen}/>
-  </Drawer.Navigator>
-);
+export const DrawerNavigator = ({props}) => {
+  
+  return(
+    <>
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>}>
+        <Drawer.Screen name='Users' component={UsersScreen}/>
+        <Drawer.Screen name='Orders' component={OrdersScreen}/>
+      </Drawer.Navigator>
+    </>
+  );
+};
 
 export default class AppNavigator extends Component{
   render(){
@@ -48,3 +100,41 @@ export default class AppNavigator extends Component{
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+  },
+  instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+  },
+  sideMenuProfileIcon: {
+    resizeMode: 'stretch',
+    width: 70,
+    height: 70,
+    borderRadius: 250 / 2,
+  },
+  title: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 10,
+  },
+  navigation:{
+    flex: 9,
+  },
+  list: {
+    flexDirection: 'column',
+    margin: 10,
+    height:60,
+    justifyContent: "center"
+  }
+});
